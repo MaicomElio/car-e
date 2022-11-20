2e0744d088c171519380037bdc6472eaff76fd9c
MaicomElio <118624710+MaicomElio@users.noreply.github.com>
2022-11-19 21:16:51 -0300

    Tela de Login feita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent {
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'SpamTuk', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [];
   constructor() {}
 }
