51854cfba2d485bbca1c771a327f62d45f6c3e3b
MaicomElio <118624710+MaicomElio@users.noreply.github.com>
2022-11-20 18:49:14 -0300

    Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'SpamTuk', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Início', url: '/folder/Inbox', icon: 'warning' },
+    { title: 'Cálculos', url: '/folder/Inbox', icon: 'warning' },
+    { title: 'Metas', url: '/folder/Outbox', icon: 'warning' },
+    { title: 'Saldos', url: '/folder/Favorites', icon: 'warning' },
+    { title: 'Extratos', url: '/folder/Archived', icon: 'warning' },
+    { title: 'Gráficos', url: '/folder/Trash', icon: 'warning' },
+    { title: 'Transações', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Investir', url: '/folder/Archived', icon: 'warning' },
+    { title: 'Educação Financeira', url: '/folder/Trash', icon: 'warning' },
+    { title: 'Contas Bancárias', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Configurações', url: '/folder/Inbox', icon: 'warning' },
   ];
-  public labels = [];
+  
   constructor() {}
 }
