27a258166e5000aa9257989ca8285b2f2ef2a30b
MaicomElio <118624710+MaicomElio@users.noreply.github.com>
2023-03-24 18:02:38 -0300

    Car-E

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/folder/Inbox', icon: 'warning' },
-    { title: 'Cálculos', url: '/folder/Inbox', icon: 'warning' },
-    { title: 'Metas', url: '/folder/Outbox', icon: 'warning' },
-    { title: 'Saldos', url: '/folder/Favorites', icon: 'warning' },
-    { title: 'Extratos', url: '/folder/Archived', icon: 'warning' },
-    { title: 'Gráficos', url: '/folder/Trash', icon: 'warning' },
-    { title: 'Transações', url: '/folder/Spam', icon: 'warning' },
-    { title: 'Investir', url: '/folder/Archived', icon: 'warning' },
-    { title: 'Educação Financeira', url: '/folder/Trash', icon: 'warning' },
-    { title: 'Contas Bancárias', url: '/folder/Spam', icon: 'warning' },
-    { title: 'Configurações', url: '/folder/Inbox', icon: 'warning' },
+    { title: 'Meus Veículos', url: '/folder/Inbox', icon: 'warning' },
+    { title: 'Histórico', url: '/folder/Outbox', icon: 'warning' },
+    { title: 'Próximas Manutenções', url: '/folder/Favorites', icon: 'warning' },
+    { title: 'Notificações', url: '/folder/Archived', icon: 'warning' },
+    { title: 'Dicas', url: '/folder/Trash', icon: 'warning' },
+    { title: 'Configurações', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Sair', url: '/folder/Archived', icon: 'warning' },
   ];
   
   constructor() {}
